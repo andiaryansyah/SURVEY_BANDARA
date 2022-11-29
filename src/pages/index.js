9a02dec7587606f55987fb7f4343f9d180cbd6e1
andiaryansyah <aariansyah70@gmail.com>
2022-11-30 03:21:51 +0800

    ganti font family jadi roboto

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,1 @@
 export {default as Landingpage } from './Landingpage/Landingpage';
-export {default as Dashboard } from './Dashboard/Dashboard';
-export {default as Pasbandara } from './PasBandara/PasBandara';
